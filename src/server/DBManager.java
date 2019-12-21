f7300061525f6ebe18cb82d6e32ae1e1dfe78217
justniu <1573996821@qq.com>
2019-12-21 13:50:13 +0800

    login query DB

--- src/server/DBManager.java
+++ src/server/DBManager.java
@@ -1,13 +1,11 @@
 package server;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBManager {
     public static Connection connection(String DBname, String name, String passwd){
         Connection con = null;
-        String uri = "jdbc:msyql://localhost:3306" + DBname + "?useSSL=true&characterEncoding=utf-8";
+        String uri = "jdbc:mysql://localhost:3306/" + DBname + "?useSSL=true&characterEncoding=utf-8";
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -21,5 +19,21 @@ public class DBManager {
         return con;
     }
 
+    public static void main(String[] args) {
+        Connection con = DBManager.connection("MyChat", "root", "niuzhuang");
+        try {
+            Statement sql = con.createStatement();
+            String sqlStr = "select * from user";
+            ResultSet rs = sql.executeQuery(sqlStr);
+            rs.next();
+            String rsName = rs.getString(1);
+            String rsPw = rs.getString(2);
+            String rsStatus = rs.getString(3);;
+            System.out.println(rsName + "\t"+ rsPw +"\t"+rsStatus);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
--- src/server/UserServices.java
+++ src/server/UserServices.java
@@ -1,118 +1,142 @@
 package server;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import common.UserStatus;
+
+import java.sql.*;
 import java.util.List;
 
 public class UserServices {
     private static int idCount = 3; //id
 
-    /** 新增用户 */
-    public void addUser(User user){
-        List<User> users = loadAllUser();
-        users.add(user);
-        saveAllUser(users);
-    }
+//    /** 新增用户 */
+//    public void addUser(User user){
+//        List<User> users = loadAllUser();
+//        users.add(user);
+//        saveAllUser(users);
+//    }
 
     /** user login */
     public boolean login(String name, String password){
         PreparedStatement preSql;
+        Statement ss;
         Connection con;
         String sqlStr;
         ResultSet rs;
-        con = DBManager.connection("MyChat", "root", "niuhzuang");
+        con = DBManager.connection("MyChat", "root", "niuzhuang");
         if(con == null) return false;
         try {
             con.setAutoCommit(false);
-            sqlStr = "select * from user where name=?, password=?";
+            sqlStr = "select * from user where name=? and password=?";
             preSql = con.prepareStatement(sqlStr);
             preSql.setString(1, name);
             preSql.setString(2, password);
             rs = preSql.executeQuery();
-
+            if(rs.next()){
+                String status = rs.getString(3);
+                if(UserStatus.valueOf(status) == UserStatus.OUTLINE){
+                    sqlStr = "update user set status='LOGINED' where name='1' and password='2'";
+//                    preSql = con.prepareStatement(sqlStr);
+//                    preSql.setString(1, "LOGINED");
+//                    preSql.setString(2, name);
+//                    preSql.setString(3, password);
+//                    int ok = preSql.executeUpdate();
+                    ss = con.createStatement();
+                    ss.executeUpdate(sqlStr);
+                    return true;
+                }
+            }
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            try {
+                con.commit();
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
         return false;
     }
 
     /** 根据ID加载用户 */
-    public User loadUser(long id){
-        User result = null;
-        List<User> users = loadAllUser();
-        for (User user : users) {
-            if(id == user.getId()){
-                result = user;
-                break;
-            }
-        }
-        return result;
-    }
-
-
-    /** 加载所有用户 */
-    @SuppressWarnings("unchecked")
-    public List<User> loadAllUser() {
-        List<User> list = null;
-        ObjectInputStream ois = null;
-        try {
-            ois = new ObjectInputStream(
-                    new FileInputStream(
-                            DataBuffer.configProp.getProperty("dbpath")));
-
-            list = (List<User>)ois.readObject();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally{
-            IOUtil.close(ois);
-        }
-        return list;
-    }
-
-    private void saveAllUser(List<User> users) {
-        ObjectOutputStream oos = null;
-        try {
-            oos = new ObjectOutputStream(
-                    new FileOutputStream(
-                            DataBuffer.configProp.getProperty("dbpath")));
-            //写回用户信息
-            oos.writeObject(users);
-            oos.flush();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally{
-            IOUtil.close(oos);
-        }
-    }
-
-
-
-    /** 初始化几个测试用户 */
-    public void initUser(){
-        User user = new User("admin", "Admin", 'm', 0);
-        user.setId(1);
-
-        User user2 = new User("123", "yong", 'm', 1);
-        user2.setId(2);
-
-        User user3 = new User("123", "anni", 'f', 2);
-        user3.setId(3);
-
-        List<User> users = new CopyOnWriteArrayList<User>();
-        users.add(user);
-        users.add(user2);
-        users.add(user3);
-
-        this.saveAllUser(users);
-    }
-
-    public static void main(String[] args){
-        new UserService().initUser();
-        List<User> users = new UserService().loadAllUser();
-        for (User user : users) {
-            System.out.println(user);
-        }
+//    public User loadUser(long id){
+//        User result = null;
+//        List<User> users = loadAllUser();
+//        for (User user : users) {
+//            if(id == user.getId()){
+//                result = user;
+//                break;
+//            }
+//        }
+//        return result;
+//    }
+//
+//
+//    /** 加载所有用户 */
+//    @SuppressWarnings("unchecked")
+//    public List<User> loadAllUser() {
+//        List<User> list = null;
+//        ObjectInputStream ois = null;
+//        try {
+//            ois = new ObjectInputStream(
+//                    new FileInputStream(
+//                            DataBuffer.configProp.getProperty("dbpath")));
+//
+//            list = (List<User>)ois.readObject();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }finally{
+//            IOUtil.close(ois);
+//        }
+//        return list;
+//    }
+//
+//    private void saveAllUser(List<User> users) {
+//        ObjectOutputStream oos = null;
+//        try {
+//            oos = new ObjectOutputStream(
+//                    new FileOutputStream(
+//                            DataBuffer.configProp.getProperty("dbpath")));
+//            //写回用户信息
+//            oos.writeObject(users);
+//            oos.flush();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }finally{
+//            IOUtil.close(oos);
+//        }
+//    }
+//
+//
+//
+//    /** 初始化几个测试用户 */
+//    public void initUser(){
+//        User user = new User("admin", "Admin", 'm', 0);
+//        user.setId(1);
+//
+//        User user2 = new User("123", "yong", 'm', 1);
+//        user2.setId(2);
+//
+//        User user3 = new User("123", "anni", 'f', 2);
+//        user3.setId(3);
+//
+//        List<User> users = new CopyOnWriteArrayList<User>();
+//        users.add(user);
+//        users.add(user2);
+//        users.add(user3);
+//
+//        this.saveAllUser(users);
+//    }
+//
+//    public static void main(String[] args){
+//        new UserService().initUser();
+//        List<User> users = new UserService().loadAllUser();
+//        for (User user : users) {
+//            System.out.println(user);
+//        }
+//    }
+    public static void main(String[] args) {
+        boolean b = new UserServices().login("1", "2");
+        System.out.println(b);
     }
 }
